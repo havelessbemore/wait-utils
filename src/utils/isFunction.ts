8672bfbb357d70775305c925d6acda6f55d66494
Michael Rojas <michael.rojas@sonymusic-pde.com>
2025-07-13 18:09:39 -0400

    Add documentation for isFunction()

--- src/utils/isFunction.ts
+++ src/utils/isFunction.ts
@@ -1,3 +1,17 @@
+/**
+ * Determines whether a given value is a function.
+ *
+ * @param value - The value to test
+ *
+ * @returns `true` if the value is a function, otherwise `false`
+ *
+ * @example
+ * ```ts
+ * isFunction(() => {}); // true
+ * isFunction(null);     // false
+ * isFunction(42);       // false
+ * ```
+ */
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export function isFunction(value: unknown): value is Function {
   return typeof value === "function";
